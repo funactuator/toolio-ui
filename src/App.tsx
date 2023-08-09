10584d808e8554207fc229cfdf708876a9f05403
Kuldeep Pandey <kuldeep.msvm@gmail.com>
2023-08-09 12:05:34 +0530

    DEV-002: Git testing changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1>This is my first ts project in react</h1>
+      <h1>This is my first ts project in</h1>
+      <h2>Testing Git</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
